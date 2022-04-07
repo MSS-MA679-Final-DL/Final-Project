d18c10338813e4b90fa0cfc087142df37d2927ca
Zening Ye <znye@bu.edu>
2022-04-07 02:07:16 -0400

    EDA Update April 7, 2022 2：07 EST

--- EDA.R
+++ EDA.R
@@ -1,5 +1,5 @@
 # load package
-pacman::p_load(tidyverse,R.matlab)
+pacman::p_load(tidyverse,R.matlab,gridExtra)
 theme_set(theme_bw())
 #-------------------------------------------------------------------------------
 # Direct Interaction
@@ -96,36 +96,38 @@ plot_zscore_ns <- function(dta){
   dta %>% ggplot() + 
     geom_col(aes(x=seq(1:nrow(dta)), y = non_social*2), col= "blanchedalmond") +
     geom_line(aes(x=seq(1:nrow(dta)), y = Average)) +
-    xlab('Reaction Time (ms)') + ylab('Z-Score')
+    labs(x = 'Reaction Time (ms)', y = 'Z-Score')
 }
 
-plot_zscore_ns(df1) + labs(title = 'Average Reaction time and non-social for Mouse in 608034_409 (Dir_Intec)')
-plot_zscore_ns(df2) + labs(title = 'Average Reaction time and non-social for Mouse in 608102_412 (Dir_Intec)')
-plot_zscore_ns(df3) + labs(title = 'Average Reaction time and non-social for Mouse in 608102_414 (Dir_Intec)')
-plot_zscore_ns(df4) + labs(title = 'Average Reaction time and non-social for Mouse in 608102_416 (Dir_Intec)')
-plot_zscore_ns(df5) + labs(title = 'Average Reaction time and non-social for Mouse in 608102_417 (Dir_Intec)')
-plot_zscore_ns(df6) + labs(title = 'Average Reaction time and non-social for Mouse in 608102_418 (Dir_Intec)')
+z_ns1 <- plot_zscore_ns(df1) #+ labs(title = 'Reaction time & non-social in 608034_409')
+z_ns2 <- plot_zscore_ns(df2) #+ labs(title = 'Reaction time & non-social in 608102_412')
+z_ns3 <- plot_zscore_ns(df3) #+ labs(title = 'Reaction time & non-social in 608102_414')
+z_ns4 <- plot_zscore_ns(df4) #+ labs(title = 'Reaction time & non-social in 608102_416')
+z_ns5 <- plot_zscore_ns(df5) #+ labs(title = 'Reaction time & non-social in 608102_417')
+z_ns6 <- plot_zscore_ns(df6) #+ labs(title = 'Reaction time & non-social in 608102_418')
 
+grid.arrange(z_ns1,z_ns2,z_ns3,z_ns4,z_ns5,z_ns6, nrow = 3)
 
 # Z-score and interaction plot----------------------------------------------------------
 plot_zscore_int <- function(dta){
   dta %>% ggplot() + 
     geom_col(aes(x=seq(1:nrow(dta)), y = interaction*2), col= "lavender") +
     geom_line(aes(x=seq(1:nrow(dta)), y = Average)) +
-    xlab('Reaction Time (ms)') + ylab('Z-Score') 
+    labs(x = 'Reaction Time (ms)', y = 'Z-Score')
 }
 
-plot_zscore_int(df1) + labs(title = 'Average Reaction time and interaction for Mouse in 608034_409 (Dir_Intec)')
-plot_zscore_int(df2) + labs(title = 'Average Reaction time and interaction for Mouse in 608102_412 (Dir_Intec)')
-plot_zscore_int(df3) + labs(title = 'Average Reaction time and interaction for Mouse in 608102_414 (Dir_Intec)')
-plot_zscore_int(df4) + labs(title = 'Average Reaction time and interaction for Mouse in 608102_416 (Dir_Intec)')
-plot_zscore_int(df5) + labs(title = 'Average Reaction time and interaction for Mouse in 608102_417 (Dir_Intec)')
-plot_zscore_int(df6) + labs(title = 'Average Reaction time and interaction for Mouse in 608102_418 (Dir_Intec)')
+z_int1 <- plot_zscore_int(df1) #+ labs(title = 'Reaction time & interaction in 608034_409')
+z_int2 <- plot_zscore_int(df2) #+ labs(title = 'Reaction time & interaction in 608102_412')
+z_int3 <- plot_zscore_int(df3) #+ labs(title = 'Reaction time & interaction in 608102_414')
+z_int4 <- plot_zscore_int(df4) #+ labs(title = 'Reaction time & interaction in 608102_416')
+z_int5 <- plot_zscore_int(df5) #+ labs(title = 'Reaction time & interaction in 608102_417')
+z_int6 <- plot_zscore_int(df6) #+ labs(title = 'Reaction time & interaction in 608102_418')
 
+grid.arrange(z_int1,z_int2,z_int3,z_int4,z_int5,z_int6, nrow = 3)
 
 # Proportion plot - interaction-------------------------------------------------
 plot_proportion_int <- function(dta){
-  pp_df <- data.frame(group = c("interacion", "none"), 
+  pp_df <- data.frame(group = c("Interacion", "None"), 
                       value = c(sum(dta$interaction == 1)/nrow(dta), sum(dta$interaction == 0)/nrow(dta)))
   ggplot(pp_df, aes(x="", y=value, fill=group)) +
     geom_bar(stat="identity", width=1) +
@@ -136,23 +138,24 @@ plot_proportion_int <- function(dta){
     theme_void()
 }
 
-plot_proportion_int(df1) + 
-  labs(title = 'The proportion of interaction for Mouse in 608034_409 (Dir_Intec)')
-plot_proportion_int(df2) + 
-  labs(title = 'The proportion of interaction for Mouse in 608102_412 (Dir_Intec)')
-plot_proportion_int(df3) + 
-  labs(title = 'The proportion of interaction for Mouse in 608102_414 (Dir_Intec)')
-plot_proportion_int(df4) + 
-  labs(title = 'The proportion of interaction for Mouse in 608102_416 (Dir_Intec)')
-plot_proportion_int(df5) + 
-  labs(title = 'The proportion of interaction for Mouse in 608102_417 (Dir_Intec)')
-plot_proportion_int(df6) + 
-  labs(title = 'The proportion of interaction for Mouse in 608102_418 (Dir_Intec)')
+p_int1 <- plot_proportion_int(df1) + 
+  labs(title = 'INT_608034_409')
+p_int2 <- plot_proportion_int(df2) + 
+  labs(title = 'INT_608102_412')
+p_int3 <- plot_proportion_int(df3) + 
+  labs(title = 'INT_608102_414')
+p_int4 <- plot_proportion_int(df4) + 
+  labs(title = 'INT_608102_416')
+p_int5 <- plot_proportion_int(df5) + 
+  labs(title = 'INT_608102_417')
+p_int6 <- plot_proportion_int(df6) + 
+  labs(title = 'INT_608102_418')
 
+grid.arrange(p_int1,p_int2,p_int3,p_int4,p_int5,p_int6, nrow = 3)
 
 # Proportion plot - non_social--------------------------------------------------
 plot_proportion_ns <- function(dta){
-  pp_df <- data.frame(group = c("non social behavor", "none"), 
+  pp_df <- data.frame(group = c("Non_social", "None"), 
                       value = c(sum(dta$non_social == 1)/nrow(dta), sum(dta$non_social == 0)/nrow(dta)))
   ggplot(pp_df, aes(x="", y=value, fill=group)) +
     geom_bar(stat="identity", width=1) +
@@ -163,19 +166,20 @@ plot_proportion_ns <- function(dta){
     theme_void()
 }
 
-plot_proportion_ns(df1) + 
-  labs(title = 'The proportion of non-social behavior for Mouse in 608034_409 (Dir_Intec)')
-plot_proportion_ns(df2) + 
-  labs(title = 'The proportion of non-social behavior for Mouse in 608102_412 (Dir_Intec)')
-plot_proportion_ns(df3) + 
-  labs(title = 'The proportion of non-social behavior for Mouse in 608102_414 (Dir_Intec)')
-plot_proportion_ns(df4) + 
-  labs(title = 'The proportion of non-social behavior for Mouse in 608102_416 (Dir_Intec)')
-plot_proportion_ns(df5) + 
-  labs(title = 'The proportion of non-social behavior for Mouse in 608102_417 (Dir_Intec)')
-plot_proportion_ns(df6) + 
-  labs(title = 'The proportion of non-social behavior for Mouse in 608102_418 (Dir_Intec)')
+p_ns1 <- plot_proportion_ns(df1) + 
+  labs(title = 'NS_608034_409')
+p_ns2 <- plot_proportion_ns(df2) + 
+  labs(title = 'NS_608102_412')
+p_ns3 <- plot_proportion_ns(df3) + 
+  labs(title = 'NS_608102_414')
+p_ns4 <- plot_proportion_ns(df4) + 
+  labs(title = 'NS_608102_416')
+p_ns5 <- plot_proportion_ns(df5) + 
+  labs(title = 'NS_608102_417')
+p_ns6 <- plot_proportion_ns(df6) + 
+  labs(title = 'NS_608102_418')
 
+grid.arrange(p_ns1,p_ns2,p_ns3,p_ns4,p_ns5,p_ns6, nrow = 3)
 
 # bar plot - interaction -------------------------------------------------------
 count_interaction <- data.frame(x = c("608034_409", "608102_412", "608102_414",
@@ -200,7 +204,7 @@ ggplot(data = count_interaction, aes(x = x, y = interaction, fill = behavior))+
   geom_col()+
   scale_fill_brewer(palette = "Purples") +
   geom_text(aes(label = round(interaction, 3)), position = position_stack(vjust = 0.5), size = 3) +
-  labs(title = "The proportion of interaction for six mouse")
+  labs(title = "The proportion of interaction for six mouse", x = 'Mouse', y = 'Percentage')
 
 
 # bar plot - non_social -------------------------------------------------------
@@ -226,7 +230,7 @@ ggplot(data = count_non_social, aes(x = x, y = non_social, fill = behavior))+
   geom_col()+
   scale_fill_brewer(palette = "Purples") +
   geom_text(aes(label = round(non_social, 3)), position = position_stack(vjust = 0.5), size = 3) +
-  labs(title = "The proportion of non-social for six mouse")
+  labs(title = "The proportion of non-social for six mouse", x = 'Mouse', y = 'Percentage')
 
 
 # bar plot 2 - interaction -------------------------------------------------------
@@ -234,7 +238,7 @@ count2_interaction <- data.frame(x = c("608034_409", "608102_412", "608102_414",
                                        "608102_416", "608102_417", "608102_418",
                                        "608034_409", "608102_412", "608102_414",
                                        "608102_416", "608102_417", "608102_418"),
-                                 behavior = c(rep("interaction", 6), rep("none", 6)),
+                                 behavior = c(rep("Interaction", 6), rep("None", 6)),
                                  interaction = c(sum(df1$interaction == 1),
                                                  sum(df2$interaction == 1),
                                                  sum(df3$interaction == 1),
@@ -252,7 +256,7 @@ ggplot(data = count2_interaction, aes(x = x, y = interaction, fill = behavior))+
   geom_col()+
   scale_fill_brewer(palette = "Purples") +
   geom_text(aes(label = round(interaction, 3)), position = position_stack(vjust = 0.5), size = 3) +
-  labs(title = "The count of interaction for six mouse")
+  labs(title = "The count of cell in interaction for six mouse", x = 'Mouse', y = 'Percentage')
 
 
 # bar plot 2 - non_social -------------------------------------------------------
@@ -260,7 +264,7 @@ count2_non_social <- data.frame(x = c("608034_409", "608102_412", "608102_414",
                                       "608102_416", "608102_417", "608102_418",
                                       "608034_409", "608102_412", "608102_414",
                                       "608102_416", "608102_417", "608102_418"),
-                                behavior = c(rep("non_social", 6), rep("none", 6)),
+                                behavior = c(rep("Non_social", 6), rep("None", 6)),
                                 non_social = c(sum(df1$non_social == 1),
                                                sum(df2$non_social == 1),
                                                sum(df3$non_social == 1),
@@ -278,4 +282,4 @@ ggplot(data = count2_non_social, aes(x = x, y = non_social, fill = behavior))+
   geom_col()+
   scale_fill_brewer(palette = "Purples") +
   geom_text(aes(label = round(non_social, 3)), position = position_stack(vjust = 0.5), size = 3) +
-  labs(title = "The count of non-social for six mouse")
+  labs(title = "The count of cell in non-social for six mouse", x = 'Mouse', y = 'Percentage')
